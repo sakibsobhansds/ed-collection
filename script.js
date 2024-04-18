484c6256a71697a52cbea8cb58e14bab5454df13
sakibsobhansds <sakibsobhan.sds@gmail.com>
2024-04-18 11:30:25 +0600

    fourteen template continue

--- script.js
+++ script.js
@@ -2,11 +2,53 @@ const swiper = new Swiper(".style-one .collection-slider", {
   slidesPerView: 6,
   spaceBetween: 24
 });
+
 const swiper2 = new Swiper(".style-two .collection-slider", {
   slidesPerView: 5.65,
   spaceBetween: 87,
   navigation: {
-    nextEl: ".swiper-button-next-new",
-    prevEl: ".swiper-button-prev-new",
+    nextEl: ".swiper-button-next-two",
+    prevEl: ".swiper-button-prev-two",
   },
+});
+
+const swiper3 = new Swiper(".style-three .collection-slider", {
+  slidesPerView: 5,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next-three",
+    prevEl: ".swiper-button-prev-three",
+  },
+});
+
+const swiper4 = new Swiper(".style-four .collection-slider", {
+  slidesPerView: 5,
+  spaceBetween: 18
+});
+
+const swiper7 = new Swiper(".style-seven .collection-slider", {
+  slidesPerView: 7,
+  spaceBetween: 12,
+  pagination: {
+    el: ".swiper-pagination.style-seven ",
+  },
+});
+
+const swiper8 = new Swiper(".style-eight .collection-slider", {
+  slidesPerView: 7,
+  spaceBetween: 12,
+  pagination: {
+    el: ".swiper-pagination.style-eight ",
+  },
+});
+const swiper9 = new Swiper(".style-nine .collection-slider", {
+  slidesPerView: 6.4,
+  spaceBetween: 45,
+  pagination: {
+    el: ".swiper-pagination.style-nine ",
+  },
+});
+const swiper11 = new Swiper(".style-eleven .collection-slider", {
+  slidesPerView: 4,
+  spaceBetween: 20
 });
